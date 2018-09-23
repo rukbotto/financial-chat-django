6961037b60413d088d1e4ae935bf10825d879e8f
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2018-09-22 23:13:58 -0500

    Add message form

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
 
 from chat.models import Profile, Room
+from chat.forms import MessageForm
 
 
 class RoomListView(LoginRequiredMixin, ListView):
@@ -12,6 +13,11 @@ class RoomListView(LoginRequiredMixin, ListView):
 class RoomDetailView(LoginRequiredMixin, DetailView):
     model = Room
     template_name = 'room_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = MessageForm()
+        return context
 
 
 # TODO: Post message (AJAX/WebSockets)
