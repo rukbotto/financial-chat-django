f1ef131707806ada8d6cc6bb7774a92f535e20c1
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2018-09-22 19:55:11 -0500

    Fix url mapping for chat profile page

--- chat/urls.py
+++ chat/urls.py
@@ -6,5 +6,5 @@ from chat import views
 urlpatterns = [
     path('rooms/', views.RoomListView.as_view(), name='room_list'),
     path('room/<str:pk>/', views.RoomDetailView.as_view(), name='room_detail'),
-    path('profile/', views.ProfileDetailView.as_view(), name='profile_detail'),
+    path('profile/<str:pk>/', views.ProfileDetailView.as_view(), name='profile_detail'),
 ]
