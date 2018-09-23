3c96f5ea79a13c3f6b979770e4ad0c8d94b253bc
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2018-09-22 19:00:05 -0500

    Configure urls

--- financial_chat_django/urls.py
+++ financial_chat_django/urls.py
@@ -1,21 +1,15 @@
-"""financial_chat_django URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as dj_auth_views
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/chat/rooms/'), name='home'),
     path('admin/', admin.site.urls),
-]
+    path('login/', dj_auth_views.LoginView.as_view(), name='login'),
+    path('logout/', dj_auth_views.LogoutView.as_view(), name='logout'),
+    path('chat/', include('chat.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
